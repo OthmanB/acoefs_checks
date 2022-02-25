97cd3be24fdb106be12b42ab303c40120ce1783b
Othman Benomar <othman.benomar@gmail.com>
2022-02-25 11:53:39 +0900

    Create eval_aj_range.py
    eval_aj_range.py is a set of functions that allows you to define what is the typical range of aj E [2,4] given a star with a rotation a1, and given a Mass, Radius and Teff
    This function has the following usecases:
       - Define the range of priors for fitting a2 and a4
       - Define the maximum reasonable ratio aj/a1 that can be hard coded in a fitting algorithm

---
+++ eval_aj_range.py
@@ -0,0 +1,127 @@
+import numpy as np
+from scipy import interpolate
+from fit_a2sig import Qlm
+from fit_a2sig import eta0_fct
+from acoefs import eval_acoefs
+from termcolor import colored
+
+def load_Alm_grids(dir_grid, gridfiles, lmax=2):
+	Alm=[]
+	if lmax>=1:
+		Alm_grid_l1=np.load(dir_grid + gridfiles[0])
+		Alm.append(Alm_grid_l1['Alm'])# Note: ftype = 'gauss' or 'gate' depends on the Alm grid file. No need to specify it
+	if lmax >=2:
+		Alm_grid_l2=np.load(dir_grid + gridfiles[1])
+		Alm.append(Alm_grid_l2['Alm'])# Note: ftype = 'gauss' or 'gate' depends on the Alm grid file. No need to specify it
+	if lmax >=3:
+		Alm_grid_l3=np.load(dir_grid + gridfiles[2])
+		Alm.append(Alm_grid_l3['Alm'])# Note: ftype = 'gauss' or 'gate' depends on the Alm grid file. No need to specify it	
+	if lmax >=1:
+		return Alm_grid_l1['theta'], Alm_grid_l1['delta'], Alm
+	else:
+		print('Warning: lmax<1. No grid can be retrieved')
+		return [], [],[]
+
+def numax_fct(Mass=1, Radius=1, Teff=5777.):
+	numax_sun=3100.
+	Teff_sun=5777.
+	numax=numax_sun * Mass/(Radius**2 *np.sqrt(Teff/Teff_sun))
+	return numax
+
+def update_min(aj, aj_ref):
+	if aj_ref > aj:
+		return aj
+	else:
+		return aj_ref
+
+def update_max(aj, aj_ref):
+	if aj_ref < aj:
+		return aj
+	else:
+		return aj_ref
+
+def eval_aj_mean_range(Mass=1., Radius=1., Teff=5777, epsilon_nl=5*1e-4, a1=1000, dir_core='/Users/obenomar/tmp/test_a2AR/'):
+	'''
+	Evaluate the full range of possible of aj E [2,4] coefficients for a given star of Mass=Mass, Radius=Radius and Teff=Teff 
+	and given an activity level epsilon_nl and a rotation a1
+	This is usefull to set the range of simulations for aj and for setting priors on aj odds coefficient while fitting
+	Note: This function **only** considers the mean value of aj over l and at numax
+	'''
+	#Dnu_sun=135.1
+	R_sun=6.96342e5 #in km
+	M_sun=1.98855e30 #in kg
+	numax=numax_fct(Mass=Mass, Radius=Radius, Teff=Teff)
+	#
+	Volume=np.pi*(Radius*R_sun)**3 * 4./3
+	rho=Mass*M_sun/Volume * 1e-12 # Conversion into g/cm-3
+	#print('rho =', rho)
+	eta0=eta0_fct(rho=rho)
+	#
+	print('numax =', numax)
+	#print('eta0  =', eta0)
+	lmax=2 # We do not consider a6... we evaluate the range for a2 and a4 only
+	dir_grids=dir_core+"/grids/gate/0.25deg_resol/" # High precision grid
+	gridfiles=['grid_Alm_1.npz', 'grid_Alm_2.npz', 'grid_Alm_3.npz'] # Fine grids
+	#
+	theta, delta, Alm= load_Alm_grids(dir_grids, gridfiles, lmax=lmax)
+	#
+	a2_min=9999
+	a2_max=-9999
+	a4_min=9999
+	a4_max=-9999
+	#nu_nlm_all=[]
+	for j in range(len(theta)):
+		for k in range(len(delta)):
+			aj=np.zeros(6)
+			for l in range(1,lmax+1):
+				nu_nlm=[]
+				for m in range(-l, l+1):
+					# perturvation from AR		
+					dnu_AR=numax*epsilon_nl*Alm[l-1][m+l][j,k] # All Alm(theta, delta) are in [j,k]
+					# perturbation from CF
+					dnu_CF=eta0*numax * (a1*1e-9)**2 * Qlm(l,m)
+					nu_nlm.append(numax + dnu_AR + dnu_CF)
+				#nu_nlm_all.append([l,nu_nlm]) # Keep memory of all groups of modes (for debug)
+				a=eval_acoefs(l, nu_nlm)
+				#print(a)
+				# Averaging over l
+				for j in range(len(aj)):
+					aj[j]=aj[j] + a[j]
+			aj=aj/lmax
+			#print('aj = ', aj)
+			a2_min=update_min(aj[1], a2_min)
+			a2_max=update_max(aj[1], a2_max)
+			a4_min=update_min(aj[3], a4_min)
+			a4_max=update_max(aj[3], a4_max)
+	#for nu in nu_nlm_all:
+	#	print("l=", nu[0], '   nu_nlm=', nu[1])
+
+	return [a2_min*1e3, a2_max*1e3], [a4_min*1e3, a4_max*1e3] # aj converted into nHz
+
+
+R_sun=6.96342e5 #in km
+M_sun=1.98855e30 #in kg
+V=np.pi*R_sun**3 * 4./3
+rho_sun=M_sun/V * 1e-12 # Conversion into g/cm-3
+# Table of M, R derived from Bellinger+2019 (https://www.aanda.org/articles/aa/pdf/2019/02/aa34461-18.pdf)
+# Only Sun, Min and Max of his range are kept
+# Teff are comming from Pinsonault+2012 or Davies+2016 (or Campante+2015)
+KIC=['6278762', 'Sun'    , '6679371' , '7199397' ]
+M=[0.753      ,   1      ,  1.570    , 1.308]
+R=[0.7588     ,   1      ,  2.222    , 2.526]
+rho=[1.741    , rho_sun  , 0.1432    ,  0.0821]
+T=[5046       , 5777.    , 6551      ,  5821.]
+epsilon_nl=5e-3
+a1=[500,  1000, 1500, 2000, 2500]
+
+print('epsilon_nl :', epsilon_nl)
+for j in range(len(M)):
+	print(colored('KIC =', 'red'), KIC[j])
+	print('Mass: ', M[j],  '   Radius: ', R[j],   '    Teff: ', T[j])
+	for dnu in a1:
+		print(colored('a1         :'+str(dnu), 'red'))
+		a2_range, a4_range=eval_aj_mean_range(Mass=M[j], Radius=R[j], Teff=T[j], epsilon_nl=epsilon_nl, a1=dnu)
+		print(colored('a2_range:', 'red'), a2_range, ' (nHz) | ', colored(100*np.abs(a2_range[0])/dnu, 'blue'),  ' , ', colored(np.abs(100*a2_range[1])/dnu, 'blue'), '  (%)')
+		print(colored('a4_range:', 'red'), a4_range, ' (nHz) | ', colored(100*np.abs(a4_range[0])/dnu, 'blue'),  ' , ', colored(np.abs(100*a4_range[1])/dnu, 'blue'), '  (%)')
+		print(colored('----', 'red'))
+
