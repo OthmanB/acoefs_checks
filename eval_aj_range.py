a924db67a21a95eee2575c2d559d2cd3f32e9a39
Othman Benomar <othman.benomar@gmail.com>
2022-02-25 18:26:23 +0900

    Update eval_aj_range.py
    Adding possibility of using numax and/or Dnu as inputs instead of M, R, Teff
    
    Usefull if one wants purely observational quantities.

--- eval_aj_range.py
+++ eval_aj_range.py
@@ -40,23 +40,61 @@ def update_max(aj, aj_ref):
 	else:
 		return aj_ref
 
-def eval_aj_mean_range(Mass=1., Radius=1., Teff=5777, epsilon_nl=5*1e-4, a1=1000, dir_core='/Users/obenomar/tmp/test_a2AR/'):
+#Assumptions: nu_max is derived from the requested Dnu_star parameter using the relation from Stello+2009. 
+#	Dnu ~ 0.263*numax^0.77 (no uncertainty implemented here)
+def numax_from_stello2009(Dnu_star):
+	# Define the frequency range for the calculation by (1) getting numax from Dnu and (2) fixing a range around numax
+	beta0=0.263; # according to Stello+2009, we have Dnu_p ~ 0.263*numax^0.77 (https://arxiv.org/pdf/0909.5193.pdf)
+	beta1=0.77; # according to Stello+2009, we have Dnu_p ~ 0.263*numax^0.77 (https://arxiv.org/pdf/0909.5193.pdf)
+	nu_max=np.power(10, np.log10(Dnu_star/beta0)/beta1)
+	return nu_max
+
+def Dnu_from_stello2009(numax):	
+	# Define the frequency range for the calculation by (1) getting numax from Dnu and (2) fixing a range around numax
+	beta0=0.263; # according to Stello+2009, we have Dnu_p ~ 0.263*numax^0.77 (https://arxiv.org/pdf/0909.5193.pdf)
+	beta1=0.77; # according to Stello+2009, we have Dnu_p ~ 0.263*numax^0.77 (https://arxiv.org/pdf/0909.5193.pdf)
+	Dnu=beta0*np.power(numax, beta1)
+	return Dnu
+
+#def eval_aj_mean_range(numax=None, Dnu=None, Mass=1, Radius=1., Teff=5777, epsilon_nl=5*1e-4, a1=1000, dir_core='/Users/obenomar/tmp/test_a2AR/'):
+def eval_aj_mean_range(numax=None, Dnu=None, Mass=None, Radius=None, Teff=None, epsilon_nl=5*1e-4, a1=1000, dir_core='/Users/obenomar/tmp/test_a2AR/'):
 	'''
 	Evaluate the full range of possible of aj E [2,4] coefficients for a given star of Mass=Mass, Radius=Radius and Teff=Teff 
 	and given an activity level epsilon_nl and a rotation a1
+	If one wants a purely observational approach, it can provides directly numax (instead of M, R, Teff) and/or Dnu
+	If only numax is provided, Dnu is determined using scaling relation with numax from Stello+2009. Same if only Dnu is provided
 	This is usefull to set the range of simulations for aj and for setting priors on aj odds coefficient while fitting
 	Note: This function **only** considers the mean value of aj over l and at numax
 	'''
-	#Dnu_sun=135.1
 	R_sun=6.96342e5 #in km
 	M_sun=1.98855e30 #in kg
-	numax=numax_fct(Mass=Mass, Radius=Radius, Teff=Teff)
+	if numax != None and Dnu == None:
+		print('Calculating Dnu(numax)...')
+		Dnu=Dnu_from_stello2009(numax)
+		print('Dnu = ', numax)
+		eta0=eta0_fct(Dnu=Dnu)
 	#
-	Volume=np.pi*(Radius*R_sun)**3 * 4./3
-	rho=Mass*M_sun/Volume * 1e-12 # Conversion into g/cm-3
-	#print('rho =', rho)
-	eta0=eta0_fct(rho=rho)
+	if numax == None and Dnu != None:
+		print('Calculating numax(Dnu)...')
+		numax=numax_from_stello2009(Dnu)
+		eta0=eta0_fct(Dnu=Dnu)
 	#
+	if numax != None and Dnu != None:
+		print('Using numax and Dnu...')
+		eta0=eta0_fct(Dnu=Dnu)
+	#	
+	if numax == None and Dnu == None and Mass != None and Radius != None and Teff !=None:
+		print('Using M,R,Teff...')
+		numax=numax_fct(Mass=Mass, Radius=Radius, Teff=Teff)
+		Volume=np.pi*(Radius*R_sun)**3 * 4./3
+		rho=Mass*M_sun/Volume * 1e-12 # Conversion into g/cm-3
+		#print('rho =', rho)
+		eta0=eta0_fct(rho=rho)
+	if numax == None and Dnu == None and (Mass == None or Radius == None or Teff == None):
+		print('Error in eval_aj_mean_range: You need to provide (M, R, Teff) or numax and/or Dnu')
+		print('                             Please read the description of the function before using it')
+		exit()
+	
 	print('numax =', numax)
 	#print('eta0  =', eta0)
 	lmax=2 # We do not consider a6... we evaluate the range for a2 and a4 only
@@ -104,43 +142,48 @@ def eval_aj_mean_range(Mass=1., Radius=1., Teff=5777, epsilon_nl=5*1e-4, a1=1000
 	return [a2_min*1e3, a2_max*1e3], [a4_min*1e3, a4_max*1e3] # aj converted into nHz
 
 
-R_sun=6.96342e5 #in km
-M_sun=1.98855e30 #in kg
-V=np.pi*R_sun**3 * 4./3
-rho_sun=M_sun/V * 1e-12 # Conversion into g/cm-3
-# Table of M, R derived from Bellinger+2019 (https://www.aanda.org/articles/aa/pdf/2019/02/aa34461-18.pdf)
-# Only Sun, Min and Max of his range are kept
-# Teff are comming from Pinsonault+2012 or Davies+2016 (or Campante+2015)
+def aj_mean_range_v1():
+	'''
+	This function is to explore specific range for some stars. It can for example be used to evaluate 
+	The prior that has to be used before fitting a star for which M, R, Teff is known.
+	'''
+	R_sun=6.96342e5 #in km
+	M_sun=1.98855e30 #in kg
+	V=np.pi*R_sun**3 * 4./3
+	rho_sun=M_sun/V * 1e-12 # Conversion into g/cm-3
+	# Table of M, R derived from Bellinger+2019 (https://www.aanda.org/articles/aa/pdf/2019/02/aa34461-18.pdf)
+	# Only Sun, Min and Max of his range are kept
+	# Teff are comming from Pinsonault+2012 or Davies+2016 (or Campante+2015)
 
-# USE THIS PART TO STUDY THE MAXIMUM POTENTIAL RANGE OF a2 and a4 FOR KEPLER OBSERVATIONS
-KIC=['6278762', 'Sun'    , '6679371' , '7199397' ]
-M=[0.753      ,   1      ,  1.570    , 1.308]
-R=[0.7588     ,   1      ,  2.222    , 2.526]
-rho=[1.741    , rho_sun  , 0.1432    ,  0.0821]
-T=[5046       , 5777.    , 6551      ,  5821.]
-epsilon_nl=5e-4
-a1=[500,  1000, 1500, 2000, 2500]
+	# USE THIS PART TO STUDY THE MAXIMUM POTENTIAL RANGE OF a2 and a4 FOR KEPLER OBSERVATIONS
+	KIC=['6278762', 'Sun'    , '6679371' , '7199397' ]
+	M=[0.753      ,   1      ,  1.570    , 1.308]
+	R=[0.7588     ,   1      ,  2.222    , 2.526]
+	rho=[1.741    , rho_sun  , 0.1432    ,  0.0821]
+	T=[5046       , 5777.    , 6551      ,  5821.]
+	epsilon_nl=5e-4
+	a1=[500,  1000, 1500, 2000, 2500]
 
-# USE THIS PART FOR GETTING SPECIFIC EXPECTED MAXIMUM RANGE FOR THE REFERENCE STAR OF SIMULATION: 16 CYG A
-# Data from Bellinger+2019 for M, R, rho. Teff from Ramirez+2009 (and Metcalfe+2012)
-# a1 is set as per the simulations and epsilon_nl is the solar value
-'''
-KIC=['12069424']
-M=[1.056]
-R=[1.213]
-T=[5825]
-rho=[1.531]
-a1=[1000]
-epsilon_nl=5e-4 # Solar Value
-print('epsilon_nl :', epsilon_nl)
-'''
-for j in range(len(M)):
-	print(colored('KIC =', 'red'), KIC[j])
-	print('Mass: ', M[j],  '   Radius: ', R[j],   '    Teff: ', T[j])
-	for dnu in a1:
-		print(colored('a1         :'+str(dnu), 'red'))
-		a2_range, a4_range=eval_aj_mean_range(Mass=M[j], Radius=R[j], Teff=T[j], epsilon_nl=epsilon_nl, a1=dnu)
-		print(colored('a2_range:', 'red'), a2_range, ' (nHz) | ', colored(100*np.abs(a2_range[0])/dnu, 'blue'),  ' , ', colored(np.abs(100*a2_range[1])/dnu, 'blue'), '  (%)')
-		print(colored('a4_range:', 'red'), a4_range, ' (nHz) | ', colored(100*np.abs(a4_range[0])/dnu, 'blue'),  ' , ', colored(np.abs(100*a4_range[1])/dnu, 'blue'), '  (%)')
-		print(colored('----', 'red'))
+	# USE THIS PART FOR GETTING SPECIFIC EXPECTED MAXIMUM RANGE FOR THE REFERENCE STAR OF SIMULATION: 16 CYG A
+	# Data from Bellinger+2019 for M, R, rho. Teff from Ramirez+2009 (and Metcalfe+2012)
+	# a1 is set as per the simulations and epsilon_nl is the solar value
+	'''
+	KIC=['12069424']
+	M=[1.056]
+	R=[1.213]
+	T=[5825]
+	rho=[1.531]
+	a1=[1000]
+	epsilon_nl=5e-4 # Solar Value
+	print('epsilon_nl :', epsilon_nl)
+	'''
+	for j in range(len(M)):
+		print(colored('KIC =', 'red'), KIC[j])
+		print('Mass: ', M[j],  '   Radius: ', R[j],   '    Teff: ', T[j])
+		for dnu in a1:
+			print(colored('a1         :'+str(dnu), 'red'))
+			a2_range, a4_range=eval_aj_mean_range(Mass=M[j], Radius=R[j], Teff=T[j], epsilon_nl=epsilon_nl, a1=dnu)
+			print(colored('a2_range:', 'red'), a2_range, ' (nHz) | ', colored(100*np.abs(a2_range[0])/dnu, 'blue'),  ' , ', colored(np.abs(100*a2_range[1])/dnu, 'blue'), '  (%)')
+			print(colored('a4_range:', 'red'), a4_range, ' (nHz) | ', colored(100*np.abs(a4_range[0])/dnu, 'blue'),  ' , ', colored(np.abs(100*a4_range[1])/dnu, 'blue'), '  (%)')
+			print(colored('----', 'red'))
 
